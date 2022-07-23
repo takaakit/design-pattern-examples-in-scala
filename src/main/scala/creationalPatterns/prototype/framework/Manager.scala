8c773fafa93947b39392c90de0532c0407a9a1d4
takaakit <takaaki.teshima.dev@gmail.com>
2022-07-23 18:11:20 +0900

    Edit comments

--- src/main/scala/creationalPatterns/prototype/framework/Manager.scala
+++ src/main/scala/creationalPatterns/prototype/framework/Manager.scala
@@ -21,7 +21,7 @@ class Manager {
   def getDisplay(displayName: String): Display = {
     // ˅
     val d: Display = this.display.apply(displayName)
-    d.createClone()
+    d.createClone()     // Create a new object by asking a concrete class to clone itself. Therefore, do not need to know the concrete Display class name.
     // ˄
   }
 
