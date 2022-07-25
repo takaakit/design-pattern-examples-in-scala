0bf9a2118c324d66b4a3c7036e4b365396b44028
takaakit <takaaki.teshima.dev@gmail.com>
2022-07-25 18:21:55 +0900

    Add comments

--- src/main/scala/structuralPatterns/adapter/PrintMessageDisplay.scala
+++ src/main/scala/structuralPatterns/adapter/PrintMessageDisplay.scala
@@ -3,6 +3,7 @@ package structuralPatterns.adapter
 
 // ˄
 
+// Adapt the MessageDisplay interface to the Print interface.
 class PrintMessageDisplay(_message: String) extends MessageDisplay(_message) with Print {
   // ˅
   
